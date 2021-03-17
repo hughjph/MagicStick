129489a40e4579629d440cb049adc38ef64b76e5
hughjph <hughjohnson2018@outlook.com>
2021-03-17 15:06:39 +0000

    no anvil dropping

--- src/main/java/me/hughjph/MagicStick/events/DropEvent.java
+++ src/main/java/me/hughjph/MagicStick/events/DropEvent.java
@@ -28,4 +28,6 @@ public class DropEvent implements Listener {
     }
 
 
+
+
 }
---
+++ src/main/java/me/hughjph/MagicStick/events/PrepAnvil.java
@@ -0,0 +1,36 @@
+package me.hughjph.MagicStick.events;
+
+import me.hughjph.MagicStick.items.MagicStickItem;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class PrepAnvil implements Listener {
+
+    @EventHandler
+    public void onPrepAnvil(PrepareAnvilEvent e){
+
+        ItemStack result = e.getResult();
+
+        ItemStack[] inCraftingItems = e.getInventory().getContents();
+        Player player = (Player) e.getView().getPlayer();
+
+
+        for(int i = 0; i<inCraftingItems.length; i++){
+            if(inCraftingItems[i] == MagicStickItem.stick()){
+
+                result.setType(null);
+
+                player.sendMessage(ChatColor.RED + "You cannot use the " + ChatColor.GOLD + "MAGIC STICK " + ChatColor.RED + "in crafting!");
+                return;
+            }
+        }
+
+
+    }
+
+
+}
