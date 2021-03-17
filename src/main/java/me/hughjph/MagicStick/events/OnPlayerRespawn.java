908e7f6d6916090f75b8e8f62c303e3e1c5e2a9d
hughjph <hughjohnson2018@outlook.com>
2021-03-17 14:53:00 +0000

    can no longer craft with the item

--- src/main/java/me/hughjph/MagicStick/events/OnPlayerRespawn.java
+++ src/main/java/me/hughjph/MagicStick/events/OnPlayerRespawn.java
@@ -1,18 +1,19 @@
 package me.hughjph.MagicStick.events;
 
-import me.hughjph.MagicStick.MagicStick;
 import me.hughjph.MagicStick.items.MagicStickItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.Inventory;
 
+import java.util.List;
 
-public class OnPlayerJoin implements Listener {
+public class OnPlayerRespawn implements Listener {
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
+    public void onPlayerRespawn(PlayerRespawnEvent e){
+
         Player player = e.getPlayer();
 
         Inventory playerInventory = player.getInventory();
@@ -23,4 +24,6 @@ public class OnPlayerJoin implements Listener {
             playerInventory.addItem(MagicStickItem.stick());
         }
     }
+
+
 }
